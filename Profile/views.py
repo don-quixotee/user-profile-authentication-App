ca2c769a693ed79adb1d1d5d63dc4f21fcdc0ceb
don-quixotee <Abhishek>
2020-05-14 18:46:22 +0530

    first commit

---
+++ Profile/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from . import models
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+
+
+def signin(request):
+    if request.user.is_authenticated:
+        return render(request, 'homepage.html')
+    if request.method == 'Post':
+        username = request.PSOT['username']
+        password = request.POST['password']
+        user= authenticate(request , username=username, password=password)
+
+        if user not in None:
+            login(request, user)
+            return redirect('/')
+        else:
+            form  = AuthenticationForm()
+            return render (request, 'signin.html', {'form':form})
+    else:
+
+            form  = AuthenticationForm()
+            return render (request, 'signin.html', {'form':form})
+
+def signout(request):
+    logout(request)
+    return redirect('/')
+
+
